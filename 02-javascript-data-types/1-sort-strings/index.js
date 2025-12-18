86a746839af3e82a11059340c7e13fc447c45d8b
Olga Denisyuk <denisykolga@yandex.ru>
2025-12-18 15:20:54 +0300

    Solution of second task - version 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -9,19 +9,19 @@ export function sortStrings(arr, param = 'asc') {
     let newArr = [...arr];
 
     if (param === 'desc') {
-        newArr.sort((a, b) => b.localeCompare(a));
+        newArr.sort((a, b) => b.localeCompare(a, ['ru', 'en']));
 
         newArr.sort(function(a, b) {
-            if (a.toLocaleLowerCase() === b.toLocaleLowerCase()) return a.localeCompare(b);
+            if (a.toLocaleLowerCase() === b.toLocaleLowerCase()) return a.localeCompare(b, ['ru', 'en']);
         });
 
         return newArr;
     }
 
-    newArr.sort((a, b) => a.localeCompare(b));
+    newArr.sort((a, b) => a.localeCompare(b, ['ru', 'en']));
 
     newArr.sort(function(a, b) {
-        if (a.toLocaleLowerCase() === b.toLocaleLowerCase()) return b.localeCompare(a);
+        if (a.toLocaleLowerCase() === b.toLocaleLowerCase()) return b.localeCompare(a, ['ru', 'en']);
     });
 
     return newArr;
